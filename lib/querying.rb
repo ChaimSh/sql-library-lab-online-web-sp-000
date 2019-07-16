283633b337bacabd23d1092f1864d27c37fc7736
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-07-16 01:55:39 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -26,7 +26,9 @@ end
 def select_series_title_with_most_human_characters
   "SELECT title FROM series
   JOIN characters ON characters.series_id = series.id
-  WHERE characters.species = 'human'"
+  WHERE characters.species = 'human'
+  ORDER BY COUNT(species) DESC
+  LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
