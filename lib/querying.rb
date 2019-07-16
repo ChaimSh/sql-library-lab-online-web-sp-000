b0d704259f940dd961acdfe72047cbe746f71f1d
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-07-16 01:15:50 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -11,8 +11,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT value, COUNT() FROM characters
-  ;"
+  "SELECT COUNT(characters.species) FROM characters;"
 end
 
 def select_name_and_series_subgenres_of_authors
