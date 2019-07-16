22f451942c55d36c4d35d6598ac2966734de0c9e
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-07-16 02:19:21 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -25,8 +25,8 @@ end
 
 def select_series_title_with_most_human_characters
   "SELECT title FROM series
-  JOIN characters ON characters.series_id = series.id;
-
+  JOIN characters ON characters.series_id = series.id
+  GROUP BY series.title, characters HAVING characters.species = 'Human'
   "
 end
 
